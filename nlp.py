0cb3034b31b228d7ca21dd8eaf9dc559cb2b6115
anth7310 <anthonyin98@hotmail.ca>
2020-06-24 19:31:50 -0400

    Moved wordcloud to nlp class

--- nlp.py
+++ nlp.py
@@ -11,7 +11,7 @@ stop_words = set(stopwords.words('english'))
 custom_stop_words = [
     'i', 'u', '\'s', 'n\'t', '``', '\'\'', '\'m', '\'ll', '\'re',
     'ur', 'im', 'ill', 'tho', '\'ve', '...', 
-    '\'d'
+    '\'d', 'i\'m', 'www', 'like'
 ]
 stop_words = stop_words.union(custom_stop_words)
 
@@ -37,40 +37,58 @@ class NLP:
     
     def sorted_vocabulary(self, token_set, min_count=10):
         """
-        print a sorted vocabulary count
+        Generator for a sorted vocabulary count
+        Excludes stopwords
         """
-        stop_words = self.stopwords
         d = [k for k, v in sorted(token_set.items(), key=lambda item: item[1])]
-        print(len(token_set))
         for k in d[::-1]:
             if k not in stop_words and k not in punctuation and token_set[k] > min_count:
                 print(k, token_set[k])
+        
+    def wordcloud(self, thread, participant=None):
+        messages = thread.messages(participant)
+        messages = [msg.lower() for msg in messages]
+        comment_words = ' '.join(messages)
+        wordcloud = WordCloud(collocations=False, stopwords=stop_words).generate(comment_words) 
+    
+        # plot the WordCloud image                        
+        plt.figure(figsize = (8, 8), facecolor = None) 
+        plt.imshow(wordcloud) 
+        plt.axis("off") 
+        plt.tight_layout(pad = 0) 
+        
+        plt.show() 
+    
+    def avg_words(self, thread, participant=None):
+        """
+        Return the average word count
+        """
+        token_set = dict()
+        for i in tqdm(range(len(thread.messages(participant)))):
+            # all tokens to lower case
+            sentence = thread.messages(participant)[i].lower()
+            tokens = word_tokenize(sentence)
+            for t in tokens:
+                if t in token_set:
+                    token_set[t] += 1
+                else:
+                    token_set[t] = 1
+        return token_set
 
 if __name__ == '__main__':
     x = Messenger()
-    x.add_thread('threads/sulay.json')
+    x.add_thread('threads/Sulay.json')
     thread_name = x.thread_names()[0]
 
-    # nlp = NLP()
-    # print(x[thread_name].participants())
-    # ['Seema Mistry', 'Anthony Inthavong']
+    nlp = NLP()
+    print(x[thread_name].participants())
     # for p in x[thread_name].participants():
-    # token_set = nlp.vocabulary_count(x[thread_name], 'Anthony Inthavong')
+    # participant = None
+    # token_set = nlp.vocabulary_count(x[thread_name], participant)
     # s = sum([v for k, v in token_set.items()])
     # avg = s/len(token_set)
     # nlp.sorted_vocabulary(token_set, int(avg))
+    # [print(k, v) for k, v in nlp.sorted_vocabulary(token_set, int(avg))]
 
-    ['Sulayman Malik', 'Anthony Inthavong']
-    comment_words = ''.join(x[thread_name].messages('Anthony Inthavong'))
-    # comment_words = [k]
-    wordcloud = WordCloud(collocations=False, stopwords=stop_words).generate(comment_words) 
-  
-    # plot the WordCloud image                        
-    plt.figure(figsize = (8, 8), facecolor = None) 
-    plt.imshow(wordcloud) 
-    plt.axis("off") 
-    plt.tight_layout(pad = 0) 
-    
-    plt.show() 
-   
+    nlp.wordcloud(x[thread_name])
     
