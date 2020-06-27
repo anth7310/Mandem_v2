eb1d18badab50b0674634c3b1e6f00a293917e38
anth7310 <anthonyin98@hotmail.ca>
2020-06-27 01:24:06 -0400

    working version of heatmap for conversation

--- Thread.py
+++ Thread.py
@@ -1,13 +1,13 @@
 import json
 from datetime import datetime
+from datetime import timedelta
 import regex
-from datetime import datetime
 from algorithms.unicodetoascii import unicodetoascii
+from Message import Message
 
 # encode to UTF-8
 from emoji import UNICODE_EMOJI
 BYTES_EMOJI = [ emoji.encode() for emoji in list(UNICODE_EMOJI) ]
-
 
 class Thread:
     def __init__(self, file_name: str):
@@ -22,11 +22,56 @@ class Thread:
 
         self.data = messages_dict
         self.title = messages_dict['title']
-
         self._messages_to_ascii()
+        self._message_list = self._create_message_list()
         
         self.messages_keys = self._generate_messages_keys()
     
+    def get_message_list(self):
+        return (str(msg) for msg in self._message_list)
+    
+    def _create_message_list(self, participant=None):
+        """
+        Return a list of Message objects in a thread
+        Only consider messages with 'content' tag
+        If participant not None, return all messages by participant
+        if group_by != None return a dict where key: date and value: list of messages
+        group_by:
+            day
+            month
+            year
+        """
+        d = self.data['messages']
+        # helper function
+        def f(x, participant=None):
+            if participant:
+                return 'content' in x and x['sender_name'] == participant
+            return 'content' in x
+
+        # encode into Message object
+        msgs = [ 
+            Message(d[i]['sender_name'], d[i]['timestamp_ms'], d[i]['content']) for i, x in enumerate(d) if f(d[i], participant)
+        ]
+        return msgs
+    
+    def group_by(self, participant=None, time='month'):
+        """
+        Group a message_list by dates: day, month, or year
+        """
+        msg_list = self._create_message_list(participant)
+        d = dict()
+        ts = msg_list[0].timestamp_ms
+        timedelta(days=1)
+        print(datetime.fromtimestamp(ts/1000.0))
+        exit()
+        for msg in msg_list:
+            if time == 'day':
+                pass
+            elif time == 'month':
+                pass
+            elif time == 'year':
+                pass
+
     def _messages_to_ascii(self):
         """
         Replace all messages with non ascii characters to ascii equivalent, utf-8 encoding
@@ -96,6 +141,13 @@ class Thread:
         if participant:
             return len([ x for i, x in enumerate(self.data['messages']) if helper(i, x) ] )
         return len(self.data['messages'])
+    
+    def average_length(self, participant=None):
+        """
+        Return the average length of messages in thread
+        If participant != None return number of messages for participant
+        """
+        pass
 
     def span(self):
         """
@@ -113,4 +165,54 @@ class Thread:
         return ''.join(c for c in s if c in BYTES_EMOJI)
 
 if __name__ == '__main__':
-    pass
+    import plotly.graph_objects as go
+    import numpy as np
+    from datetime import datetime
+    from datetime import timedelta
+    
+
+    # np.random.seed(1)
+    x = Thread('./threads/mandam.json')
+    print(x.span())
+    msg_list = x._create_message_list()
+
+    participants = sorted(x.participants(), reverse=True)
+    dates = [datetime.fromtimestamp(msg.timestamp_ms/1000.0) for msg in msg_list]
+
+    # count number messages per participant
+    # create matrix using dictionary to store all values
+    dmys = [(date.year, date.month, date.day) for date in dates]
+    # print(sorted(list(set(dmys))))
+    d = dict()
+    for p in participants:
+        d[p] = dict()
+        for date in set(dmys):
+            d[p][date] = 0
+
+    for msg in msg_list:
+        date = datetime.fromtimestamp(msg.timestamp_ms/1000.0)
+        # keep count by day, month, year
+        dmy = (date.year,  date.month, date.day)
+        p = msg.sender_name
+        if p == 'Richard Zhen':
+            continue
+        if dmy in d[p]:
+            d[p][dmy] += 1
+
+    # message count for each date
+    z = []
+    for p in participants:
+        z.append([ d[p][v] for v in sorted(d[p].keys(), reverse=False) ])
+    # print(z[p])
+    dates = sorted([datetime(year, month, day) for (year, month, day) in set(dmys)])
+    fig = go.Figure(data=go.Heatmap(
+            z=z,
+            x=dates,
+            y=participants,
+            colorscale='Viridis'))
+
+    fig.update_layout(
+        title='Messages sent per day in {}'.format(x.title),
+        xaxis_nticks=36) # 36
+
+    fig.show()
